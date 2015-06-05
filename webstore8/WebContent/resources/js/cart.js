437a9e722d48eca19b2eb9afd7c7b1e36d9566d0
Janardhan Bonu <janardhanbonu@gmaill.com>
2015-06-05 15:07:32 -0500

    -u

---
+++ webstore8/WebContent/resources/js/cart.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+	
+	var cartId;
+	
+	addToCart = function(productId){
+			$.ajax({
+		 		url: '/webstore8/rest/cart/add/' + productId,
+		 		type: 'PUT',
+				success: function(response){
+	 		 		alert("Product Successfully added to the Cart!");
+
+				},
+				error: function(){						
+					alert('Error while request..');
+				}
+			});
+		}
+
+	   removeFromCart = function(productId) {
+			$.ajax({
+				url: '/webstore8/rest/cart/remove/'+ productId,
+		 		type: 'PUT',
+			 	 success: function (response) {
+ 
+		 		 		location.reload(true);
+
+					},
+					error: function(){						
+						alert('Error while request..');
+			 	 } 
+		   });
+	   }
+
+		 getCartId = function( ) {
+				$.ajax({
+					url: '/webstore8/rest/cart/getId/',
+					type: 'GET',
+					success : function(id) {
+					 		 		 cartId = id;
+					 	 		}
+			});
+		 }
+
+		 refreshCart = function() {
+				$.ajax({
+					url: '/webstore8/rest/cart/'+ cartId,
+					type: 'GET',
+					dataType: "json",
+					success : function(cartData) {
+ 		 		 		location.reload(true);
+ 	 
+					 }
+			});
+		 }
+
+});
+
+ 
